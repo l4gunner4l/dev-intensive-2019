6f623ceaac74e95aefcb62d472aad2afecc6284c
Nikola <155nimix@mail.ru>
2019-07-06 19:39:46 +0300

    in User.Builder deleted strings with lastVisit

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -2,7 +2,10 @@ package ru.skillbranch.devintensive.extensions
 
 fun String.truncate(length:Int = 16): String{
     val firstString = this.trim()
-    return if (length < firstString.length)
-                "${this.substring(0, length).trimEnd()}..."
+    return if (length < firstString.length) "${this.substring(0, length).trimEnd()}..."
            else firstString
+}
+
+fun String.stripHtml():String{
+    return this
 }
