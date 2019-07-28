d198e2fcdf33f7d40014e7a7382ab5c0734dfb03
Nikola <155nimix@mail.ru>
2019-07-29 01:00:17 +0300

    video done

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -1,6 +1,5 @@
 package ru.skillbranch.devintensive.ui.profile
 
-import android.graphics.Color
 import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
@@ -10,8 +9,12 @@ import android.view.View
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
+import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 class ProfileActivity : AppCompatActivity(){
 
@@ -21,13 +24,15 @@ class ProfileActivity : AppCompatActivity(){
 
     var isEditMode = false
     lateinit var viewFields : Map<String, TextView>
+    private lateinit var viewModel: ProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         Log.d("M_MainActivity", "onCreate")
-
+        Log.d("M_ProfileActivity", "${isEditMode}")
         initViews(savedInstanceState)
+        initViewModel()
     }
 
     override fun onStart() {
@@ -71,6 +76,26 @@ class ProfileActivity : AppCompatActivity(){
         outState?.putBoolean(IS_EDIT_MODE, isEditMode)
     }
 
+    private fun initViewModel(){
+        viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
+        viewModel.getProfileData().observe(this, Observer{ updateUI(it) })
+        viewModel.getTheme().observe(this, Observer{ updateTheme(it) })
+    }
+
+    private fun updateTheme(mode: Int) {
+        Log.d("M_ProfileActivity","updateTheme")
+        delegate.setLocalNightMode(mode)
+    }
+
+    private fun updateUI(profile: Profile) {
+        profile.toMap().also {
+            for((k,v) in viewFields){
+                v.text = it[k].toString()
+            }
+        }
+
+    }
+
     private fun initViews(savedInstanceState: Bundle?) {
         viewFields = mapOf(
             "nickname" to tv_nick_name,
@@ -82,11 +107,20 @@ class ProfileActivity : AppCompatActivity(){
             "rating" to tv_rating,
             "respect" to tv_respect
         )
+
         isEditMode = savedInstanceState?.getBoolean(IS_EDIT_MODE,false) ?: false
         showCurrentMode(isEditMode)
+
         btn_edit.setOnClickListener{
+            if (isEditMode) saveProfileInfo()
             isEditMode = !isEditMode
             showCurrentMode(isEditMode)
+            Log.d("M_ProfileActivity", "isEditMode ${isEditMode}")
+            Log.d("M_ProfileActivity", "et_first_name.isEnabled ${et_first_name.isEnabled}")
+        }
+
+        btn_switch_theme.setOnClickListener{
+            viewModel.switchTheme()
         }
     }
 
@@ -109,11 +143,22 @@ class ProfileActivity : AppCompatActivity(){
                     PorterDuff.Mode.SRC_IN
                 ) }
                 else null
-            val icon = if (isEdit){ resources.getDrawable(R.drawable.ic_save_black_24dp) }
-                       else { resources.getDrawable(R.drawable.ic_edit_black_24dp) }
+            val icon = if (isEdit){ resources.getDrawable(R.drawable.ic_save_black_24dp, theme) }
+                       else { resources.getDrawable(R.drawable.ic_edit_black_24dp, theme) }
 
             background.colorFilter = filter
             setImageDrawable(icon)
+        }
+    }
+
+    private fun saveProfileInfo(){
+        Profile(
+            firstName = et_first_name.text.toString(),
+            lastName = et_last_name.text.toString(),
+            about = et_about.text.toString(),
+            repository = et_repository.text.toString()
+        ).apply {
+            viewModel.saveProfileData(this)
         }
     }
 }
