00f8dc5df63ae0bd936ced719b7e576be06910a7
Nikola <155nimix@mail.ru>
2019-07-31 21:00:14 +0300

    Regex modified

--- app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -22,7 +22,21 @@ class ExampleUnitTest {
 
     @Test
     fun test_fun() {
-
+        println("валидно - " + "https://github.com/johnDoe".isValidGitHubUrl())
+        println("валидно - " + "https://www.github.com/johnDoe".isValidGitHubUrl())
+        println("валидно - " + "www.github.com/johnDoe".isValidGitHubUrl())
+        println("валидно - " + "github.com/johnDoe".isValidGitHubUrl())
+        println("валидно - " + "github.com/john-Doe".isValidGitHubUrl())
+        println("невалидно - " + "github.com/johnDo-e".isValidGitHubUrl())
+        println("невалидно - " + "github.com/johnDo-".isValidGitHubUrl())
+        println("невалидно - " + "https://anyDomain.github.com/johnDoe".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com/".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com/johnDoe/tree".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com/johnDoe/tree/something".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com/enterprise".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com/pricing".isValidGitHubUrl())
+        println("невалидно - " + "https://github.com/join".isValidGitHubUrl())
     }
 
 
