b1ea54d30811935e1de21c6b37739716970b355b
Nikola <155nimix@mail.ru>
2019-09-06 22:06:47 +0300

    lastMessageShort()

--- app/src/main/java/ru/skillbranch/devintensive/models/data/Chat.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/data/Chat.kt
@@ -3,6 +3,7 @@ package ru.skillbranch.devintensive.models.data
 import ru.skillbranch.devintensive.data.CacheManager
 import ru.skillbranch.devintensive.extensions.shortFormat
 import ru.skillbranch.devintensive.models.BaseMessage
+import ru.skillbranch.devintensive.models.TextMessage
 import ru.skillbranch.devintensive.utils.DataGenerator
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
@@ -47,19 +48,21 @@ data class Chat(
     }
 
     fun unreadableMessageCount(): Int {
-        //TODO
         return 7
     }
 
     private fun lastMessageDate(): Date? {
-        //TODO
-        return Date()
+        return messages.last().date
     }
 
-    private fun lastMessageShort(): Pair<String, String> {
-        //TODO
-        return "No messages" to "Nikola"
-    }
+    /*private fun lastMessageShort(): Pair<String, String> {
+        val lastFrom = messages.last().from?.firstName ?: "-"
+        val lastMsg= if (messages.lastOrNull() is TextMessage) messages.last().text
+        else messages.last().image
+        return lastMsg to lastFrom
+    }*/
+    private fun lastMessageShort(): Pair<String, String> = "msg" to "from me"
+
     private fun isSingle() = members.size == 1
 
     enum class ChatType{
